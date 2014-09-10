b2e7e9dedb9ea0037609191ebfbb42c008de790a
Zehao Zhang <zehaozhang@wl-25-66.cine.claremont.edu>
2014-09-10 16:54:56 -0700

    Initial Commit

---
+++ ViewTest/ZZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZZAppDelegate.h
+//  ViewTest
+//
+//  Created by Zehao Zhang on 14-9-10.
+//  Copyright (c) 2014年 Zehao Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ViewTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ViewTest
+//
+//  Created by Zehao Zhang on 14-9-10.
+//  Copyright (c) 2014年 Zehao Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZZAppDelegate class]));
+    }
+}
